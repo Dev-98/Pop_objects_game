2f9bd4961b661421a104628c7f837cf791958dc1
Dev-98 <devparker48@gmail.com>
2023-03-05 09:02:43 +0530

    First

---
+++ game.py
@@ -0,0 +1,110 @@
+import pygame, sys, os
+import random
+import datetime
+
+
+def game_stream():
+    index =[]
+    width = 600
+    height = 700
+    white = (255,255,255)
+    black = (0,0,0)
+    clock = pygame.time.Clock()
+    g = 1
+    score = 0
+    fps = 20
+    life = 10 
+    fruits = ['purple', 'spaceship',  'virus', 'purple', 'bubble', 'rocket']
+    # count = 0
+    pygame.init()
+    # Background
+    BG = pygame.transform.scale(pygame.image.load(os.path.join("C:\\Users\\DELL\\Desktop\\Hackhound\\code\\", "background-black.png")), (600, 800))
+    # game display
+    gameDisplay = pygame.display.set_mode((width, height),pygame.RESIZABLE)
+    gameDisplay.blit(BG, (0,0))
+
+
+    font = pygame.font.Font('C:\\Users\\DELL\\Desktop\\Hackhound\\code\\comic.ttf', 32)
+    score_text = font.render(str(score), True, white)
+    # life_left = font.render(str(life), True, white )
+
+    def generate_random_fruits(fruit):
+        path =f'C:\\Users\\DELL\\Desktop\\Hackhound\\code\\{fruit}.png'
+        data[fruit] = {
+            'img' : pygame.transform.scale(pygame.image.load(path),(80, 80)),
+            'x' : random.randint(0, 500),
+            'y' : random.randint(0, 500),
+            'speed_x' : random.randint(-2, 2),
+            'speed_y' : random.randint(-2, 2),
+            'throw' : False,
+            't' : 1,
+            'hit' : False,
+        }
+
+        if(random.random() >= 0.95):
+            data[fruit]['throw'] = True
+        else:
+            data[fruit]['throw'] = False
+    # genrate random
+    data = {}
+    for fruit in fruits:
+        generate_random_fruits(fruit)
+        
+    # display update
+    pygame.display.update()
+    # infinit loop 
+    
+    while True:        
+
+        gameDisplay.blit(BG, (0,0))
+        # gameDisplay.fill(white)
+        gameDisplay.blit(score_text, (0,0))
+        for key,value in data.items():
+            if value['throw']:
+                value['x'] = value['x'] + value['speed_x']
+                value['y'] = value['y'] + 15
+                value['speed_y'] += (g*value['t'])
+                value['t'] += 0.2
+
+                if value['y'] <= 700:
+                    gameDisplay.blit(value['img'], (value['x'],value['y']))
+                else:
+                    generate_random_fruits(key)
+
+                current_position = pygame.mouse.get_pos()
+                if not value['hit'] and current_position[0] > value['x'] and current_position[0] < value['x']+60 and current_position[1] > value['y'] and current_position[1] < value['y']+60:
+                    path = os.path.join("C:\\Users\\DELL\\Desktop\\Hackhound\\code\\"+key+'.png')
+                    value['img'] = pygame.transform.scale(pygame.image.load(path), (0, 0))
+                    value['speed_x'] += 1
+                    score += 1
+                    score_text = font.render(str(score), True, white)
+                    life_left = font.render(str(life), True, white )
+                    # index.append(pygame.mouse.get_pos())
+                    value['hit'] = True
+
+               
+                    
+                # if value["hit"]:
+                #     playsound.playsound("sound.wav")
+                    
+
+            else:
+                generate_random_fruits(key)
+                
+
+        pygame.display.update()
+        clock.tick(fps)
+
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+if __name__=='__main__':
+    # now = datetime.datetime.now()
+    # current_time = now.strftime("%S")
+    # cond = 
+    # if int(current_time) <= int
+
+    game_stream()
+    # sys.exit()
